bb682c5720bc01895b303e26cb27214cfeea7d1c
闫帅 <sartisty@gmail.name>
2015-01-24 17:47:01 +0800

    修复view dashboard存在的bug

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,60 +1,54 @@
-var Script = function() {
-  // sidebar dropdown menu auto scrolling
-  jQuery('#sidebar .sub-menu > a').click(function() {
-    var o = ($(this).offset());
-    diff = 250 - o.top;
-    if (diff > 0)
-      $("#sidebar").scrollTo("-=" + Math.abs(diff), 500);
-    else
-      $("#sidebar").scrollTo("+=" + Math.abs(diff), 500);
-  });
+$.fn.extend({
+  initDateRangePicker: function () {
+    var $drp = $(this);
+    var $elem = $drp.find('span');
+    $elem.html(moment().subtract('days', 7).format('YYYY/MM/DD')
+        + ' - ' + moment().subtract('days', -1).format('YYYY/MM/DD'));
+    $drp.daterangepicker(
+        {
+          opens: 'left',
+          locale: {
+            applyLabel: '&#x786e;&#x8ba4;',
+            cancelLabel: '&#x53d6;&#x6d88;',
+            fromLabel: '&#x8d77;&#x59cb;&#x65e5;&#x671f;',
+            toLabel: '&#x7ed3;&#x675f;&#x65e5;&#x671f;',
+            weekLabel: 'W',
+            customRangeLabel: '&#x81ea;&#x5b9a;&#x4e49;&#x65e5;&#x671f;&#x8303;&#x56f4;',
+            daysOfWeek: ['&#x65e5;', '&#x4e00;', '&#x4e8c;', '&#x4e09;', '&#x56db;', '&#x4e94;', '&#x516d;'],
+            monthNames: ['1&#x6708;', '2&#x6708;', '3&#x6708;', '4&#x6708;', '5&#x6708;', '6&#x6708;', '7&#x6708;', '8&#x6708;', '9&#x6708;', '10&#x6708;', '11&#x6708;', '12&#x6708;'],
+            firstDay: 0
+          },
+          startDate: moment().subtract('days', 7),
+          endDate: moment().subtract('days', -1),
+          maxDate: moment().subtract('days', -1)
+        },
+    function (start, end) {
+      $elem.html(start.format('YYYY/MM/DD') + ' - ' + end.format('YYYY/MM/DD'));
+    });
+  },
+  initBackToTop: function () {
+    var backToTop = $('<a>', {
+      id: 'back-to-top',
+      href: '#top',
+      html: '<i class="fa fa-chevron-circle-up"></i>'
+    });
+    backToTop.appendTo('body');
+    backToTop.hide();
 
-  $(function() {
-    function responsiveView() {
-      var wSize = $(window).width();
-      if (wSize <= 768) {
-        $('#container').addClass('sidebar-close');
-        $('#sidebar > ul').hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 150) {
+        backToTop.fadeIn();
+      } else {
+        backToTop.fadeOut();
       }
+    });
 
-      if (wSize > 768) {
-        $('#container').removeClass('sidebar-close');
-        $('#sidebar > ul').show();
-      }
-    }
-    $(window).on('load', responsiveView);
-    $(window).on('resize', responsiveView);
-  });
+    backToTop.click(function (e) {
+      e.preventDefault();
 
-  $('.fa-bars').click(function() {
-    if ($('#sidebar').is(":visible") === true) {
-      $('#main-content').css({
-        'margin-left': '0px'
-      });
-      $('#sidebar').css({
-        'margin-left': '-210px'
-      });
-      $('#sidebar').hide();
-      $("#container").addClass("sidebar-closed");
-      $('.header').removeData('pin');
-      $('.nav-wrapper').pin();
-      var width = $('.nav-wrapper').width();
-      $('.nav-wrapper').width(width + 210);
-      $(window).resize();
-    } else {
-      $('#main-content').css({
-        'margin-left': '210px'
-      });
-      $('#sidebar').show();
-      $('#sidebar').css({
-        'margin-left': '0'
-      });
-      $("#container").removeClass("sidebar-closed");
-      $('.nav-wrapper').removeData('pin');
-      var width = $('.nav-wrapper').width();
-      $('.nav-wrapper').width(width - 210);
-      $('.header').pin();
-      $(window).resize();
-    }
-  });
-}();
+      $('body, html').animate({
+        scrollTop: 0
+      }, 600);
+    });
+  }
+});
